074712419edaa7b7415b6993b9be5d7b752ba0cc
bohdan145 <huckier78@gmail.com>
2020-05-17 14:07:04 +0300

    update styles

--- src/style.js
+++ src/style.js
@@ -2,8 +2,7 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1,
-    backgroundColor: "#00000077"
+    flex: 1
   },
   mask: {
     flex: 1,
@@ -11,9 +10,11 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: "#fff",
-    width: "100%",
-    height: 0,
-    overflow: "hidden"
+    width: "100%"
+  },
+  back: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, .55)"
   },
   draggableContainer: {
     width: "100%",
